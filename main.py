af00c039a70149cdcf9121342c4ec33d25f8edfb
Taylor Allen <taylorallen0913@gmail.com>
2022-11-01 16:52:54 -0700

    done

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import random
 
-# ******************************
-# Make your Code
-# ******************************
+numbers = []
+
+for i in range(10):
+    numbers.append(random.randint(0, 1000))
+
+smallest_index = 0
+
+for i in range(len(numbers)):
+    if numbers[i] < numbers[smallest_index]:
+        smallest_index = i
+
+print(numbers[smallest_index])
+
+print(smallest_index)
+
+print(min(numbers))
+
+print(numbers.index(min(numbers)))
